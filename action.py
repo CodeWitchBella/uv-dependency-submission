a40f6b40ac23a45768f162cb4908c83d49c182b2
Isabella Skořepová <isabella@skorepova.info>
2026-04-22 18:28:51 +0200

    only retry on some status codes

--- action.py
+++ action.py
@@ -7,6 +7,7 @@ import datetime
 import json
 import os
 import subprocess
+import sys
 from time import sleep
 import tomllib
 from typing import Any
@@ -68,19 +69,22 @@ def retrying_check_output(cmd: list[str], *, input: str) -> str:
     sleep_for = 1
     last_err: Exception = ValueError("Invalid number of retries")
     retries = 5
+    retryable_statuses = ('500', '502')
     for i in range(retries):
         if i != 0:
             print(f"{RED}Command failed, retrying in {sleep_for} seconds ({i + 1}/{retries})...{END}")
             sleep(sleep_for)
             sleep_for = min(30, sleep_for * 2)
         try:
-            output = subprocess.check_output(cmd, input=input, stderr=subprocess.PIPE, universal_newlines=True)
+            output = subprocess.check_output(cmd, input=input, stderr=sys.stderr, universal_newlines=True)
             print("Dependency submission successful!")
             return output
         except subprocess.CalledProcessError as e:
             last_err = e
             print(e.output)
-            print(json.loads(e.output))
+            body = json.loads(e.output)
+            if body["status"] not in retryable_statuses:
+                raise
 
     print(f"{RED}Command failed, no more retries{END}")
     raise last_err
